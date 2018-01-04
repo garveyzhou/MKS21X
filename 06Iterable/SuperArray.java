842ee70bbacb64b69923732e009557c5f0a35f0a
unknown <gzhou2@stuy.edu>
2018-01-04 11:16:18 -0500

    fixed add and typos

--- 06Iterable/SuperArray.java
+++ 06Iterable/SuperArray.java
@@ -11,7 +11,8 @@ public class SuperArray implements Iterable<String>{
     public SuperArray(int start){
 	data = new String[start];
 	size = 0;
-    }	
+    }
+    
     public void clear(){
 	data = new String[data.length];
 	size = 0;
@@ -38,6 +39,9 @@ public class SuperArray implements Iterable<String>{
 	return res += "]";
     }
     public String get(int index){
+	if(index < 0 || index > size()){
+	    throw new IndexOutOfBoundsException();
+	}
 	return data[index];
     }
     public String set(int index,String element){
@@ -45,13 +49,15 @@ public class SuperArray implements Iterable<String>{
 	    String old = data[index];
 	    data[index] = element;
 	    return old;}
-
+	//throw new UnsupportedOperationException()
+	return "Error";
+    }
     private void resize(){
 	String[] newdata = new String[data.length*2+1];
 	for (int x = 0;x<data.length;x++){
 	    newdata[x] = data[x];
 	}
-        data = newdata;
+	data = newdata;
     }
     public String[] getData(){
 	return data;
@@ -64,7 +70,7 @@ public class SuperArray implements Iterable<String>{
 	}
 	return false;
     }
-    public int indexOf(String element){
+    private int indexOf(String element){
 	for(int i =0; i < data.length ; i ++){
 	    if (data[i].equals(element) ){
 		return i;
@@ -72,7 +78,7 @@ public class SuperArray implements Iterable<String>{
 	}
 	return -1;
     }
-    public int LastindexOf(String element){
+    private int LastindexOf(String element){
 	for ( int i = data.length-1; i >= 0;i -= 1){
 	    if (data[i].equals(element)){
 		return i;
@@ -81,22 +87,25 @@ public class SuperArray implements Iterable<String>{
 	return -1;
     }
     public void add(int index, String element){
-	int x  = size;
-	if (size >=  data.length){
-	    System.out.println("Error,Index OutofBounds");
+	int x  = size();
+	if ( index < 0 || index > x){
+	    throw new IndexOutOfBoundsException();
 	}
-	else{while(x != index){
+	else{
+	    if(size == data.length){
+		resize();
+	    }
+	    while(x >index){
 		data[x] = data[x-1];
-		x -= 1;
+		x--;
 	    }
 	    data[x] = element;
 	    size += 1;
 	}
     }
     public String remove(int x){
-	if (x > data.length ||  x < 0){
-	    System.out.println("Error,Index OutOfBounds");
-	    return "null";
+	if (x > size() ||  x < 0){
+	    throw new IndexOutOfBoundsException();
 	}
 	String removed = "";
 	String[] newdata = new String[data.length];
@@ -119,5 +128,5 @@ public class SuperArray implements Iterable<String>{
 	return false;}
     public Iterator<String> iterator(){
 	return new SuperArrayIterator(this,0);
-}
+    }
 }
